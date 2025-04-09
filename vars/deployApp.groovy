fb94cca806f1a0964f918b2b2b428471774b3c65
hruthingali <hruthingd123@gmail.com>
2025-04-09 15:34:28 +0530

    Delete vars/deployApp.groovy

--- vars/deployApp.groovy
+++
@@ -1,6 +0,0 @@
-def call(String environment = 'staging') {
-    stage('Deploy') {
-        echo "Deploying application to ${environment}..."
-        sh "echo Deploying to ${environment} server"
-    }
-}
