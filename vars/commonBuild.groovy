94b0fc1750e9da349223a4f3d3ce1214bcb97068
hruthingali <hruthingd123@gmail.com>
2025-04-09 15:48:28 +0530

    Update commonBuild.groovy

--- vars/commonBuild.groovy
+++ vars/commonBuild.groovy
@@ -1,14 +1,18 @@
 def call() {
-    stage('Checkout') {
-        checkout scm
-    }
-    stage('Unit Tests') {
-        echo 'Running unit tests...'
-        sh './gradlew test'
-    }
-    stage('Deploy') {
-        echo 'Deploying to environment...'
-        // Replace this with actual deploy logic, e.g.:
-        // sh './deploy.sh'
+    node {
+        stage('Checkout') {
+            checkout scm
+        }
+
+        stage('Unit Tests') {
+            echo 'Running unit tests...'
+            sh './gradlew test'
+        }
+
+        stage('Deploy') {
+            echo 'Deploying to environment...'
+            // Add actual deployment logic here
+        }
     }
 }
+
